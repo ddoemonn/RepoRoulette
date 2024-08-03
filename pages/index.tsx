77614c585864b371ce851b7b508652e417b9d8ce
ozer <ozer@90pixel.com>
2024-08-03 22:44:36 +0300

    ADD: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { Inter } from 'next/font/google';
+import { Exo } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const exo = Exo({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <h2>RepoRoulette</h2>
+    <main className={`flex min-h-screen flex-col items-center bg-black text-white justify-center p-24 ${exo.className}`}>
+      <h2 className="font-bold text-4xl">RepoRoulette 🎲</h2>
+      <p className="mt-3 text-lg text-center mx-auto max-w-xl">
+        Discover random, fascinating repositories on GitHub. Click the button below to get started and explore new projects!
+      </p>
+      <button className="mt-6 p-2  bg-white text-black font-semibold rounded-lg hover:bg-red-500 hover:border-red-500 hover:text-white  hover:shadow-none transition duration-300">
+        Show Me a Repo
+      </button>
     </main>
   );
 }
